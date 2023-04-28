b6e8a7e4ce14c42d41832bafc6751f8daf1f7ef9
pgm1964 <pgm1964@gepplus.com>
2023-04-28 17:40:03 +0900

    Ch5 - Navbar : page72

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,50 +1,56 @@
 type NavItemProps = {
-    title: string
-    url: string
-}
-
-function NavItem({ title, url }: NavItemProps) {
+  title: string;
+  url: string;
+  isSelected: boolean;
+};
+function NavItem(props: NavItemProps) {
+  const { title, url, isSelected } = props
   return (
     <li>
-      <a className="block px-3 py-2 transition hover:text-teal-500"
-        href={url}>{title}</a>
+      <a className={`block px-3 py-2 transition hover:text-teal-500 
+      ${isSelected ? "text-teal-500" : ""}`} href={url}>{title}</a>
     </li>
   )
-} 
-
-/*
-function NavItem(props: NavItemProps) {
-    //const { title, url } = props
-    return (
-        <li>
-            <a className="block px-3 py-2 transition hover:text-teal-500"
-          href={props.url}>{props.title}</a>
-        </li>
-    )
 }
-*/
 
-function Navbar() {
-    return (
-        <div className="flex justify-center mx-auto max-w-7xl h-16 pt-6">
-            <nav>
-                <ul className="flex rounded-full bg-white/90 px-3 text-sm font-
+type NavbarProps = {
+  pageId: string;
+};
+function Navbar(props: NavbarProps) {
+  return (
+    <div className="flex justify-center mx-auto max-w-7xl h-16 pt-6">
+      <nav>
+        <ul
+          className="flex rounded-full bg-white/90 px-3 text-sm font-
 medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 
-backdrop-blur">
-                    <NavItem title={"About Me"} url={"/"} />
-                    <NavItem title={"Projects"} url={"/projects"} />
-                    <NavItem title={"Essays"} url={"/essays"} />
-                </ul>
-            </nav>
-        </div>
-    )
+backdrop-blur"
+        >
+          <NavItem
+            title={"About Me"}
+            url={"/"}
+            isSelected={props.pageId == "about_me"}
+          />
+          <NavItem
+            title={"Projects"}
+            url={"/projects"}
+            isSelected={props.pageId == "projects"}
+          />
+          <NavItem
+            title={"Essays"}
+            url={"/essays"}
+            isSelected={props.pageId == "essays"}
+          />
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default function Layout({ children }: any) {
-    return (
-        <>
-            <Navbar />
-            <main>{children}</main>
-        </>
-    )
-} 
+  return (
+    <>
+      <Navbar pageId={children.props.pageId} />
+      <main>{children}</main>
+    </>
+  );
+}
