04415df3876c1d4c981ecdef2e6ef66e0395de2f
pgm1964 <pgm1964@gepplus.com>
2023-04-28 16:57:21 +0900

    Ch5 - Creating A Layout 3

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,11 @@
+function NavItem() { 
+  return ( 
+    <li> 
+      <a className="block px-3 py-2 transition hover:text-teal-500" 
+href="/">About Me</a> 
+    </li> 
+  ) 
+} 
 function Navbar() { 
   return ( 
     <div className="flex justify-center mx-auto max-w-7xl h-16 pt-6"> 
@@ -5,15 +13,15 @@ function Navbar() {
         <ul className="flex rounded-full bg-white/90 px-3 text-sm font-
 medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 
 backdrop-blur"> 
-          <li> 
-            <a className="block px-3 py-2 transition hover:text-teal-500" 
-href="/">About Me</a> 
-          </li> 
+          <NavItem /> 
+          <NavItem /> 
+          <NavItem />  
         </ul> 
       </nav> 
     </div> 
   ) 
 } 
+
 export default function Layout({ children }: any) { 
   return ( 
     <> 
